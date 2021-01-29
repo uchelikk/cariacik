807dd0c89899815ed546cf55be4cb88a93cc1507
uchelikk <uchelikk@gmail.com>
2021-01-29 20:49:45 +0300

    user.controller deleted

--- src/controllers/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import User, { IUser } from '../schema/users';
-
-export default class UserController {
-  static addNewUser = async (user: Partial<IUser>) => {
-    const result = await new User(user);
-
-    return result;
-  };
-}
